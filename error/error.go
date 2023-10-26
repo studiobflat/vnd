b478ddbe93036a23887423b80a9e3d308075a18f
HaoLe <hao.le@tesuji.io>
2023-10-26 22:12:44 +0700

    feat: add custom error

--- error/error.go
+++ error/error.go
@@ -2,6 +2,7 @@ package error
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,4 +15,26 @@ type Error struct {
 
 func (e *Error) Error() string {
 	return fmt.Sprintf("CustomCode=%d, %s", e.CustomCode, e.HTTPError.Error())
+}
+
+func InternalServerError(err error) Error {
+	return Error{
+		CustomCode: -50011,
+		HTTPError: &echo.HTTPError{
+			Code:     http.StatusInternalServerError,
+			Message:  err.Error(),
+			Internal: err,
+		},
+	}
+}
+
+func BadRequest(err error) Error {
+	return Error{
+		CustomCode: -40011,
+		HTTPError: &echo.HTTPError{
+			Code:     http.StatusBadRequest,
+			Message:  err.Error(),
+			Internal: err,
+		},
+	}
 }
